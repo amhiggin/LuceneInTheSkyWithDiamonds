49837531dcc3fa6ae59cb82dd85fedddfbc804a6
Jibin Xavier <xavierj@tcd.ie>
2018-03-14 11:18:07 +0000

    added important tags

--- src/com/lucene_in_the_sky_with_diamonds/document/FBISLoader.java
+++ src/com/lucene_in_the_sky_with_diamonds/document/FBISLoader.java
@@ -1,4 +1,5 @@
 package com.lucene_in_the_sky_with_diamonds.document;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -12,7 +13,6 @@ import org.apache.lucene.document.Field;
 import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
 
-
 public class FBISLoader {
 	private String fileName;
 	private List<Document> collectionDocuments;
@@ -21,82 +21,75 @@ public class FBISLoader {
 		this.fileName = fileName;
 		this.collectionDocuments = new ArrayList<Document>();
 	}
-	private String topLevelRegex = "<DOCNO>|</DOCNO>|"
-									+ "<HT>|</HT>|"
-									+ "<HEADER>|</HEADER>|"
-									+ "<TEXT>|</TEXT>|"
-									+ "</DOC>|"
-									+ "<DATE1>|</DATE1>|"
-									+ "<H2>| </H2>|"
-									+ "<H3>|</H3>|"
-									+ "<TI>|</TI>";
-	
+
+	private String topLevelRegex = "<DOCNO>|</DOCNO>|" + "<HT>|</HT>|" + "<HEADER>|</HEADER>|" + "<TEXT>|</TEXT>|"
+			+ "</DOC>|" + "<DATE1>|</DATE1>|" + "<H2>| </H2>|" + "<H3>|</H3>|" + "<TI>|</TI>";
+
 	private String headerLevelRegex = "<H2>|<DATE1>|<H3>";
+
 	public void loadDocumentsFromFile() {
-		 
-		try { 
+
+		try {
 			Scanner scan = new Scanner(new File(this.fileName));
 			scan.useDelimiter(Pattern.compile("<DOC>"));
 			while (scan.hasNext()) {
-				
-				 
+
 				String[] sections = scan.next().split(topLevelRegex);
 				ArrayList<String> cleanedData = clearEmptySections(sections);
 				System.out.println(cleanedData.size());
 				System.out.println(cleanedData);
-				
+
 				Document doc = createDocument(cleanedData);
-				
+
 				this.collectionDocuments.add(doc);
-				
-				 
-				 
+
 				break;
-			}	
+			}
 			scan.close();
-			 
+
 		} catch (Exception e) {
 			System.out.println(String.format("An exception occurred when parsing the documents from %s", fileName));
 			e.printStackTrace();
 		}
 	}
-	private ArrayList<String> clearEmptySections(String [] sections){
+
+	private ArrayList<String> clearEmptySections(String[] sections) {
 		ArrayList<String> cleanedData = new ArrayList<String>();
-		for (String sec: sections) {
-			if (sec.trim().equals("" )) { // there might be a lot empty strings after regex splitting
+		for (String sec : sections) {
+			if (sec.trim().equals("")) { // there might be a lot empty strings after regex splitting
 				;
-			}else {
+			} else {
+
 				cleanedData.add(sec.trim());
 			}
 		}
 		return cleanedData;
-		
+
 	}
-	
+
 	private Document createDocument(ArrayList<String> sections) {
-		
+
 		/*
-		 *  Ignoring HT, dont know what on earth that is.
-		 *  There is a summary section might be a good idea to use it (index 6 of sections)
+		 * Ignoring HT, dont know what on earth that is. There is a summary section
+		 * might be a good idea to use it (index 6 of sections)
+		 * 
+		 * TODO: remove tags such <f> or use the function from github
 		 */
-		
+
 		Document document = new Document();
-		
-		document.add(new StringField("docNo",sections.get(0), Field.Store.YES));
-		
+
+		document.add(new StringField("docNo", sections.get(0), Field.Store.YES));
+
 		document.add(new TextField("H2", sections.get(2), Field.Store.YES));
 		document.add(new TextField("Date", sections.get(3), Field.Store.YES));
 		document.add(new TextField("DocumentType", sections.get(4), Field.Store.YES));
 		document.add(new TextField("Title", sections.get(5), Field.Store.YES));
-	 
-		 
-		document.add(new TextField("Text", sections.get(6), Field.Store.YES));
-		 
-		
 
+		document.add(new TextField("Text", sections.get(6), Field.Store.YES));
 
 		return document;
 	}
+
 	public int getSizeOfCollection() {
 		return collectionDocuments.size();
 	}
