41b3ddd038153c629134d0f8405f2a5b870b8aef
Jibin Xavier <xavierj@tcd.ie>
2018-03-16 16:52:51 +0000

    fixed the issue with one document

---
+++ com/lucene_in_the_sky_with_diamonds/document/fbis/FBISFieldsObject.java
@@ -0,0 +1,54 @@
+package com.lucene_in_the_sky_with_diamonds;
+
+public class FBISFieldsObject {
+	
+	private String docNo = "";
+	private String text = "";
+	private String date = "";
+	private String title ="";
+	private String ftags = "";
+	private String unknownSect = "";
+	
+	
+ 
+	public String getUnknownSect() {
+		return unknownSect;
+	}
+	public void setUnknownSect(String unknownSect) {
+		this.unknownSect = this.unknownSect + unknownSect;
+	}
+	public String getFtags() {
+		return ftags;
+	}
+	public void setFtags(String ftags) {
+		// appends all the f tags into one
+		this.ftags = this.ftags +ftags ;
+	}
+	public String getDocNo() {
+		return docNo;
+	}
+	public void setDocNo(String docNo) {
+		this.docNo = docNo;
+	}
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	 
+ 
+
+}
--- com/lucene_in_the_sky_with_diamonds/document/fbis/FBISLoader.java
+++ com/lucene_in_the_sky_with_diamonds/document/fbis/FBISLoader.java
@@ -1,8 +1,14 @@
-package com.lucene_in_the_sky_with_diamonds.document;
+package com.lucene_in_the_sky_with_diamonds;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -13,89 +19,223 @@ import org.apache.lucene.document.Field;
 import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
 
+ 
+
 public class FBISLoader {
-	private String fileName;
+	/*
+	 * fb496178 is an example  stops in the middle for some reason for text
+	 */
 	private List<Document> collectionDocuments;
 
-	public FBISLoader(String fileName) {
-		this.fileName = fileName;
+	public FBISLoader() {
+
 		this.collectionDocuments = new ArrayList<Document>();
 	}
+ 
 
-	private String topLevelRegex = "<DOCNO>|</DOCNO>|" + "<HT>|</HT>|" + "<HEADER>|</HEADER>|" + "<TEXT>|</TEXT>|"
-			+ "</DOC>|" + "<DATE1>|</DATE1>|" + "<H2>| </H2>|" + "<H3>|</H3>|" + "<TI>|</TI>";
+	public void loadDocumentsFromFile(String fileName) {
 
-	private String headerLevelRegex = "<H2>|<DATE1>|<H3>";
-
-	public void loadDocumentsFromFile() {
-
-		try {
-			Scanner scan = new Scanner(new File(this.fileName));
-			scan.useDelimiter(Pattern.compile("<DOC>"));
-			while (scan.hasNext()) {
-
-				String[] sections = scan.next().split(topLevelRegex);
-				ArrayList<String> cleanedData = clearEmptySections(sections);
-				System.out.println(cleanedData.size());
-				System.out.println(cleanedData);
-
-				Document doc = createDocument(cleanedData);
-
-				this.collectionDocuments.add(doc);
-
-				break;
-			}
-			scan.close();
+		try { 
+			InputStream stream = Files.newInputStream(Paths.get(fileName));
+			BufferedReader reader = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8));
+			 
+			String line;
+		    while (!(line = getNextDoc(reader)).equals("")) {
+		         
+				ArrayList <String>sections =applyRegex(line);; 
+  
+				FBISFieldsObject docInfo = createFBISFieldsObject(sections);  
+				Document doc = createDocument(docInfo);
+//				printFBISFieldsObject(docInfo);
+				this.collectionDocuments.add(doc); 
+				 
+		    }
+			
+ 
+			
+			System.out.println(fileName+" Doc size (running total)"+ this.collectionDocuments.size());
+		 
 
 		} catch (Exception e) {
 			System.out.println(String.format("An exception occurred when parsing the documents from %s", fileName));
 			e.printStackTrace();
 		}
 	}
+	 
 
-	private ArrayList<String> clearEmptySections(String[] sections) {
-		ArrayList<String> cleanedData = new ArrayList<String>();
-		for (String sec : sections) {
-			if (sec.trim().equals("")) { // there might be a lot empty strings after regex splitting
-				;
-			} else {
-
-				cleanedData.add(sec.trim());
+	private ArrayList<String> applyRegex(String str) {
+		/*
+		 * 
+		 */
+		String topDocRegx = buildToplevelRegex();
+		String nestedRegx = nestedRegex();
+		ArrayList<String> result = new ArrayList<String>();
+		
+		String [] sections = str.split(topDocRegx);
+		for( String sec: sections) { 
+			String [] nestedSplit = null;
+			if(! sec.trim().isEmpty()) {
+				nestedSplit = sec.split(nestedRegx); 
+			} 
+			if(nestedSplit == null ) {
+				if  (!sec.trim().equals("")){
+					result.add(sec);
+				} 
 			}
+			else {
+				 
+				for(String nSec :nestedSplit ) {
+					if(!sec.trim().equals("")) {
+						result.add(nSec);
+					}
+				}
+				
+			}
+			
+			
 		}
-		return cleanedData;
+		
+		 
+		
+		
+		return result;
+		
+	}
+	private String nestedRegex() {
+		/*
+		 * TO be used after doing toplevel
+		 */
+		String regex =   FIBSFieldTypes.F_START.fieldType ;
+		return regex;
+	}
+	private String buildToplevelRegex() {
+		String regex = FIBSFieldTypes.DOC_NO_START.fieldType 
+				+ "|" + FIBSFieldTypes.HEADER_START.fieldType
+				+ "|" + FIBSFieldTypes.TEXT_START.fieldType 
+				+ "|" + FIBSFieldTypes.TITLE_START.fieldType
+				
+				+ "|" + FIBSFieldTypes.HT_START.fieldType 
+				+ "|" + FIBSFieldTypes.DATE1_START.fieldType;
 
+			//+ "|" + FIBSFieldTypes.F_START.fieldType 
+		return regex;
 	}
 
-	private Document createDocument(ArrayList<String> sections) {
+	private String getNextDoc(BufferedReader reader) throws IOException {
+		/**
+		 * Reads in each doc at a time
+		 */
+		StringBuilder doc = new StringBuilder();
+		String line = null;
+		while ((line = reader.readLine()) != null) {
+			doc.append(line);
+			if(line.equals(FIBSFieldTypes.DOC_END.fieldType)) {
+				doc.append(line);
+				break;
+			}
+			
+			
+		}
+		return doc.toString();
+		
+	}
+
+	public static String stripTags(String buf) {
+
+		return buf.replaceAll("<[^>]*>", " ");
+	}
+
+	private FBISFieldsObject createFBISFieldsObject(ArrayList<String> sections) {
+		/*
+		 * Date is in fb496178 Sometimes date appears inside the header
+		 * 
+		 * Whatever is left after parsing is the  unknownSect.
+		 * Need to do research into what to do with
+		 */
+		FBISFieldsObject obj = new FBISFieldsObject();
+	 
+		for (String sec : sections) {
+
+			if (sec.trim().contains("</DATE1>")) {
+
+				obj.setDate((sec.split("</DATE1>")[0].trim()));
+			}  
+			else if (sec.trim().contains("</DOCNO>")) {
+				
+				obj.setDocNo(sec.split("</DOCNO>")[0].trim());
+				
+			}  else if (sec.trim().contains("</TI>")) {
+
+				obj.setTitle((sec.split("</TI>")[0].trim()));
+				
+			}  else if (sec.trim().contains("</TEXT>")) {
+				// remove all the other headers
+		 
+				String cleanData = stripTags(sec.split("</TEXT>")[0].trim());
+				
+				obj.setText(cleanData);
+			}else if (sec.trim().contains("</F>")) {
+				// append all the f tags and
+			 
+				 String [] splitData = sec.split("</F>");
+				 if(splitData.length>0) {
+					 obj.setFtags((sec.split("</F>")[0].trim()));
+					 
+				 }
+					 
+				
+			 
+
+			} else {
+				/// keeps all the uninteresting tags into this
+				String cleanData = stripTags(sec.trim());
+
+				obj.setUnknownSect(cleanData);
+				 
+			}
+		}
+		return obj;
+
+	}
+	private void printFBISFieldsObject(FBISFieldsObject sections) {
+		System.out.println("DocNo "+ sections.getDocNo() + "\n\n***************");
+		System.out.println("Date "+ sections.getDate() + "\n\n***************");
+		System.out.println("Title "+ sections.getTitle() + "\n\n***************");
+		System.out.println("Text "+ sections.getText() + "\n\n***************");
+		System.out.println("Ftags "+ sections.getFtags() + "\n\n***************");
+		System.out.println("unknownSect "+ sections.getFtags() + "\n\n***************");
+	}
+	private Document createDocument(FBISFieldsObject sections) throws Exception {
 
 		/*
 		 * Ignoring HT, dont know what on earth that is. There is a summary section
 		 * might be a good idea to use it (index 6 of sections)
 		 * 
-		 * TODO: remove tags such <f> or use the function from github
+		 *  
 		 */
+		if (sections==null) {
+			throw new Exception("Cannot add empty sections to a document!");
+		}
 
 		Document document = new Document();
+		
+		
+		document.add(new StringField("DocNo", sections.getDocNo(), Field.Store.YES));
 
-		document.add(new StringField("docNo", sections.get(0), Field.Store.YES));
+ 
+		document.add(new TextField("Date", sections.getDate(), Field.Store.YES)); 
+		document.add(new TextField("Title",sections.getTitle(), Field.Store.YES));
 
-		document.add(new TextField("H2", sections.get(2), Field.Store.YES));
-		document.add(new TextField("Date", sections.get(3), Field.Store.YES));
-		document.add(new TextField("DocumentType", sections.get(4), Field.Store.YES));
-		document.add(new TextField("Title", sections.get(5), Field.Store.YES));
-
-		document.add(new TextField("Text", sections.get(6), Field.Store.YES));
+		document.add(new TextField("Text", sections.getText(), Field.Store.YES));
+		document.add(new TextField("Ftags", sections.getFtags(), Field.Store.YES));
+		document.add(new TextField("unknownSect", sections.getUnknownSect(), Field.Store.YES));
+		
 
 		return document;
 	}
 
 	public int getSizeOfCollection() {
 		return collectionDocuments.size();
-	}
-
-	public String getFileName() {
-		return fileName;
 	}
 
 	public List<Document> getCollectionDocuments() {
