5e07da891c8e614aec89647c862c1a48a3a44d97
amhiggin <amhiggin@tcd.ie>
2018-03-22 14:22:10 +0000

    Updating the script to allow passing of a custom filename based on its relative path to the script. Instructions included in the script.

--- run_search_engine.sh
+++ run_search_engine.sh
@@ -1,5 +1,15 @@
 #/bin/bash
 
-#SCORING_MODEL="$1"
-#ANALYZER="$2"
-java -jar "$(pwd)"/target/Lucene_in_The_Sky_With_Diamonds-0.0.1-SNAPSHOT.jar "qrels"
+#######################################################
+# When running this script, specify the RELATIVE path #
+# of the qrels file. 				      #
+# For example, if the qrels file is located in        #
+#    		./qrels/trec-qrels		      #
+# then specify					      #
+#    		qrels/trec-qrels		      #
+# as the argument for this script.	      	      #
+#######################################################
+
+QRELS_FILE="$1"
+
+java -jar "$(pwd)"/target/Lucene_in_The_Sky_With_Diamonds-0.0.1-SNAPSHOT.jar "$(pwd)"/"$QRELS_FILE"
