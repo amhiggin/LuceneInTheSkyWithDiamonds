5e01d3c98a0ed17114c670d6951910d7ca70d8c7
amhiggin@tcd.ie <amhiggin@tcd.ie>
2018-04-03 11:55:06 +0100

    Adding missing implementation for allowing dynamic selection of analyzer at runtime. Added the runtime  creation of output results files based on the analyzer and scoring model used. Updated the pom to include the dependency for the morfologik analyzer.

--- src/main/java/com/lucene_in_the_sky_with_diamonds/ApplicationLibrary.java
+++ src/main/java/com/lucene_in_the_sky_with_diamonds/ApplicationLibrary.java
@@ -1,6 +1,12 @@
 package com.lucene_in_the_sky_with_diamonds;
 
 import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.core.KeywordAnalyzer;
+import org.apache.lucene.analysis.core.SimpleAnalyzer;
+import org.apache.lucene.analysis.core.StopAnalyzer;
+import org.apache.lucene.analysis.core.WhitespaceAnalyzer;
+import org.apache.lucene.analysis.morfologik.MorfologikAnalyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.misc.SweetSpotSimilarity;
 import org.apache.lucene.search.similarities.BM25Similarity;
 import org.apache.lucene.search.similarities.BooleanSimilarity;
@@ -8,35 +14,78 @@ import org.apache.lucene.search.similarities.ClassicSimilarity;
 import org.apache.lucene.search.similarities.LMDirichletSimilarity;
 import org.apache.lucene.search.similarities.Similarity;
 
+import com.lucene_in_the_sky_with_diamonds.analysis.CustomAnalyzer;
+import com.lucene_in_the_sky_with_diamonds.analysis.CustomAnalyzerB;
+import com.lucene_in_the_sky_with_diamonds.analysis.CustomAnalyzerC;
+import com.lucene_in_the_sky_with_diamonds.analysis.ShingleAnalyzer;
+
 public class ApplicationLibrary {
 
-  public ApplicationLibrary() {}
+	public ApplicationLibrary() {
+	}
 
-  Similarity determineScoringModel(String scoringModel) {
-    Similarity similarityModel = null;
-    switch (scoringModel) {
-      case Constants.BM25:
-        similarityModel = new BM25Similarity();
-        break;
-      case Constants.VSM:
-        similarityModel = new ClassicSimilarity();
-        break;
-      case Constants.BOOLEAN:
-        similarityModel = new BooleanSimilarity();
-        break;
-      case Constants.LM_DIRICHLET:
-        similarityModel = new LMDirichletSimilarity();
-        break;
-      case Constants.SWEET_SPOT:
-        similarityModel = new SweetSpotSimilarity();
-        break;
-    }
-    return similarityModel;
-  }
+	Similarity determineScoringModel(String scoringModel) throws Exception {
+		Similarity similarityModel = null;
+		switch (scoringModel) {
+		case Constants.BM25:
+			similarityModel = new BM25Similarity();
+			break;
+		case Constants.VSM:
+			similarityModel = new ClassicSimilarity();
+			break;
+		case Constants.BOOLEAN:
+			similarityModel = new BooleanSimilarity();
+			break;
+		case Constants.LM_DIRICHLET:
+			similarityModel = new LMDirichletSimilarity();
+			break;
+		case Constants.SWEET_SPOT:
+			similarityModel = new SweetSpotSimilarity();
+			break;
+		default:
+			throw new Exception(String.format("Invalid scoring model specified: %s", scoringModel));
+		}
+		return similarityModel;
+	}
 
-  Analyzer determineAnalyzer(String analyzer) {
-    return null;
+	Analyzer determineAnalyzer(String parsedAnalyzer) throws Exception {
+		Analyzer analyzer = null;
+		switch (parsedAnalyzer) {
+		case Constants.KEYWORD_ANALYZER:
+			analyzer = new KeywordAnalyzer();
+			break;
+		case Constants.MORFOLOGIK_ANALYZER:
+			analyzer = new MorfologikAnalyzer();
+			break;
+		case Constants.SHINGLE_ANALYZER:
+			analyzer = new ShingleAnalyzer(StandardAnalyzer.ENGLISH_STOP_WORDS_SET);
+			break;
+		case Constants.STANDARD_ANALYZER:
+			analyzer = new StandardAnalyzer(StandardAnalyzer.ENGLISH_STOP_WORDS_SET);
+			break;
+		case Constants.SIMPLE_ANALYZER:
+			analyzer = new SimpleAnalyzer();
+			break;
+		case Constants.STOP_ANALYZER:
+			analyzer = new StopAnalyzer(StandardAnalyzer.ENGLISH_STOP_WORDS_SET);
+			break;
+		case Constants.WHITESPACE_ANALYZER:
+			analyzer = new WhitespaceAnalyzer();
+			break;
+		case Constants.CUSTOM_ANALYZER_A:
+			analyzer = new CustomAnalyzer();
+			break;
+		case Constants.CUSTOM_ANALYZER_B:
+			analyzer = new CustomAnalyzerB(StandardAnalyzer.ENGLISH_STOP_WORDS_SET);
+			break;
+		case Constants.CUSTOM_ANALYZER_C:
+			analyzer = new CustomAnalyzerC(StandardAnalyzer.ENGLISH_STOP_WORDS_SET);
+			break;
+		default:
+			throw new Exception(String.format("Invalid analyzer specified: %s", analyzer));
+		}
+		return analyzer;
 
-  }
+	}
 
 }
--- src/main/java/com/lucene_in_the_sky_with_diamonds/Constants.java
+++ src/main/java/com/lucene_in_the_sky_with_diamonds/Constants.java
@@ -4,51 +4,51 @@ import java.nio.file.Paths;
 
 public class Constants {
 
-  // Filepaths for document collections
-  static final String APPLICATION_PATH = Paths.get("").toAbsolutePath().toString();
-  static final String FT_FILEPATH = String.format("%s/input_data/ft/", APPLICATION_PATH);
-  static final String FBIS_FILEPATH = String.format("%s/input_data/fbis/", APPLICATION_PATH);
-  static final String FR94_FILEPATH = String.format("%s/input_data/fr94/", APPLICATION_PATH);
-  static final String LATIMES_FILEPATH = String.format("%s/input_data/latimes/", APPLICATION_PATH);
-  static final String TOPICS_FILEPATH =
-      String.format("%s/input_data/topics.401-450", APPLICATION_PATH);
+	// Filepaths for document collections
+	static final String APPLICATION_PATH = Paths.get("").toAbsolutePath().toString();
+	static final String FT_FILEPATH = String.format("%s/input_data/ft/", APPLICATION_PATH);
+	static final String FBIS_FILEPATH = String.format("%s/input_data/fbis/", APPLICATION_PATH);
+	static final String FR94_FILEPATH = String.format("%s/input_data/fr94/", APPLICATION_PATH);
+	static final String LATIMES_FILEPATH = String.format("%s/input_data/latimes/", APPLICATION_PATH);
+	static final String TOPICS_FILEPATH = String.format("%s/input_data/topics.401-450", APPLICATION_PATH);
 
-  // Field names for documents
+	// Field names for documents
 
-  static final String DOC_NO_STRING = "DocNo";
-  static final String TEXT_STRING = "Text";
-  static final String HEADLINE_STRING = "Headline";
+	static final String DOC_NO_STRING = "DocNo";
+	static final String TEXT_STRING = "Text";
+	static final String HEADLINE_STRING = "Headline";
 
-  // Scoring models
-  static final String VSM = "vsm";
-  static final String BM25 = "bm25";
-  static final String BOOLEAN = "boolean";
-  static final String LM_DIRICHLET = "dirichlet";
-  static final String SWEET_SPOT = "sweetspot";
-  static final String[] ALL_SCORING_MODELS = {VSM, BM25, BOOLEAN, LM_DIRICHLET, SWEET_SPOT};
+	// Scoring models
+	static final String VSM = "vsm";
+	static final String BM25 = "bm25";
+	static final String BOOLEAN = "boolean";
+	static final String LM_DIRICHLET = "dirichlet";
+	static final String SWEET_SPOT = "sweetspot";
+	static final String[] ALL_SCORING_MODELS = { VSM, BM25, BOOLEAN, LM_DIRICHLET, SWEET_SPOT };
 
-  // Analyzers
-  static final String CUSTOM_ANALYZER = "custom";
-  static final String SHINGLE_ANALYZER = "shingle";
-  static final String STANDARD_ANALYZER = "standard";
-  static final String SIMPLE_ANALYZER = "simple";
-  static final String WHITESPACE_ANALYZER = "whitespace";
-  static final String KEYWORD_ANALYZER = "keyword";
-  static final String STOP_ANALYZER = "stop";
-  static final String MORFOLOGIK_ANALYZER = "morfologik";
-  static final String[] ALL_ANALYZERS =
-      {CUSTOM_ANALYZER, STANDARD_ANALYZER, STOP_ANALYZER, SIMPLE_ANALYZER, WHITESPACE_ANALYZER,
-          KEYWORD_ANALYZER, SHINGLE_ANALYZER, MORFOLOGIK_ANALYZER};
+	// Analyzers
+	static final String CUSTOM_ANALYZER_A = "custom_A";
+	static final String CUSTOM_ANALYZER_B = "custom_B";
+	static final String CUSTOM_ANALYZER_C = "custom_C";
+	static final String SHINGLE_ANALYZER = "shingle";
+	static final String STANDARD_ANALYZER = "standard";
+	static final String SIMPLE_ANALYZER = "simple";
+	static final String WHITESPACE_ANALYZER = "whitespace";
+	static final String KEYWORD_ANALYZER = "keyword";
+	static final String STOP_ANALYZER = "stop";
+	static final String MORFOLOGIK_ANALYZER = "morfologik";
+	static final String[] ALL_ANALYZERS = { CUSTOM_ANALYZER_A, CUSTOM_ANALYZER_B, CUSTOM_ANALYZER_C, STANDARD_ANALYZER,
+			STOP_ANALYZER, SIMPLE_ANALYZER, WHITESPACE_ANALYZER, KEYWORD_ANALYZER, SHINGLE_ANALYZER,
+			MORFOLOGIK_ANALYZER };
 
-  // Strings for string formatting
-  static final String USAGE_EXCEPTION_STRING =
-      "%s args passed.\nUsage: <QRELS-FILE> <SCORING_MODEL> <ANALYZER>\n"
-          + "Options: [PATH] [%s|%s|%s|%s|%s], [%s|%s|%s|%s|%s|%s|%s|%s].";
+	// Strings for string formatting
+	static final String USAGE_EXCEPTION_STRING = "%s args passed.\nUsage: <QRELS-FILE> <SCORING_MODEL> <ANALYZER>\n"
+			+ "Options: [PATH] [%s|%s|%s|%s|%s], [%s|%s|%s|%s|%s|%s|%s|%s|%s|%s].";
 
-  static String usageExceptionMessage(String[] args) {
-    return String.format(USAGE_EXCEPTION_STRING, args.length, VSM, BM25, BOOLEAN, LM_DIRICHLET,
-        SWEET_SPOT, SIMPLE_ANALYZER, STANDARD_ANALYZER, WHITESPACE_ANALYZER, STOP_ANALYZER,
-        CUSTOM_ANALYZER, SHINGLE_ANALYZER, KEYWORD_ANALYZER, MORFOLOGIK_ANALYZER);
-  }
+	static String usageExceptionMessage(String[] args) {
+		return String.format(USAGE_EXCEPTION_STRING, args.length, VSM, BM25, BOOLEAN, LM_DIRICHLET, SWEET_SPOT,
+				SIMPLE_ANALYZER, STANDARD_ANALYZER, WHITESPACE_ANALYZER, STOP_ANALYZER, CUSTOM_ANALYZER_A,
+				CUSTOM_ANALYZER_B, CUSTOM_ANALYZER_C, SHINGLE_ANALYZER, KEYWORD_ANALYZER, MORFOLOGIK_ANALYZER);
+	}
 
 }
