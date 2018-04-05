f1a630487ff3ed080c7586da2a27cff183cb8277
amhiggin <amhiggin@tcd.ie>
2018-04-05 09:06:20 +0000

    Adding newest output run from AWS instance. Added results for the LMJelinekMercer scoring model, and made updates to scripts to reflect this. Current highest score appears to be 29.86ish. This doesn't include the narrative parsing that Jibin has since done. Removed all references to the keyword analyser in the results generation - still hasn't been removed as an option from the code.

--- generate_all_trec.sh
+++ generate_all_trec.sh
@@ -10,7 +10,7 @@ echo "Generating trec results for $SCORING_MODEL"
 "$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-standard-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-standard-results.txt
 "$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-stop-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-stop-results.txt
 "$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-shingle-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-shingle-results.txt
-"$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-keyword-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-keyword-results.txt
+#"$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-keyword-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-keyword-results.txt
 "$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-whitespace-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-whitespace-results.txt
 "$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-morfologik-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-morfologik-results.txt
 
@@ -23,7 +23,7 @@ SCORING_MODEL="bm25"
 "$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-standard-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-standard-results.txt
 "$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-stop-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-stop-results.txt
 "$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-shingle-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-shingle-results.txt
-"$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-keyword-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-keyword-results.txt
+#"$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-keyword-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-keyword-results.txt
 "$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-whitespace-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-whitespace-results.txt
 "$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-morfologik-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-morfologik-results.txt
 
@@ -35,7 +35,7 @@ SCORING_MODEL="boolean"
 "$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-standard-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-standard-results.txt
 "$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-stop-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-stop-results.txt
 "$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-shingle-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-shingle-results.txt
-"$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-keyword-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-keyword-results.txt
+#"$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-keyword-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-keyword-results.txt
 "$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-whitespace-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-whitespace-results.txt
 "$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-morfologik-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-morfologik-results.txt
 
@@ -47,7 +47,7 @@ SCORING_MODEL="dirichlet"
 "$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-standard-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-standard-results.txt
 "$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-stop-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-stop-results.txt
 "$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-shingle-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-shingle-results.txt
-"$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-keyword-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-keyword-results.txt
+#"$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-keyword-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-keyword-results.txt
 "$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-whitespace-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-whitespace-results.txt
 "$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-morfologik-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-morfologik-results.txt
 
@@ -59,6 +59,18 @@ SCORING_MODEL="sweetspot"
 "$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-standard-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-standard-results.txt
 "$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-stop-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-stop-results.txt
 "$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-shingle-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-shingle-results.txt
-"$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-keyword-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-keyword-results.txt
+#"$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-keyword-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-keyword-results.txt
+"$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-whitespace-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-whitespace-results.txt
+"$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-morfologik-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-morfologik-results.txt
+
+SCORING_MODEL="lmjm"
+"$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-custom_A-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-custom_A-results.txt
+"$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-custom_B-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-custom_B-results.txt
+"$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-custom_C-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-custom_C-results.txt
+"$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-simple-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-simple-results.txt
+"$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-standard-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-standard-results.txt
+"$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-stop-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-stop-results.txt
+"$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-shingle-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-shingle-results.txt
+#"$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-keyword-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-keyword-results.txt
 "$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-whitespace-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-whitespace-results.txt
 "$(pwd)"/trec_eval/trec_eval "$(pwd)"/qrels.assignment2.part1 "$(pwd)"/output/$SCORING_MODEL-morfologik-results.txt >> "$(pwd)"/output/trec_eval-$SCORING_MODEL-morfologik-results.txt
