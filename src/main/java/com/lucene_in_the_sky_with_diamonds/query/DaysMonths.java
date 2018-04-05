a1acee95a414491b57e38aea801aa44e69018a20
bannonco <bannonco@10.6.12.41>
2018-04-05 16:22:31 +0100

    adding query date parsing (currently not used)

---
+++ src/main/java/com/lucene_in_the_sky_with_diamonds/query/DaysMonths.java
@@ -0,0 +1,10 @@
+package com.lucene_in_the_sky_with_diamonds.query;
+
+public class DaysMonths {
+	static final String [] MONTHS = {"January","February","March","April","May","June","July","August","Septmeber","October",
+			"November","December"};
+	static final String [] MONTHS_ABBR = {"Jan","Feb","Mar","Apr","May","Jun","June","Jul","July","Aug","Sep","Sept","Oct",
+			"Nov","Dec"};
+	static final String [] DAYS = {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+	static final String [] DAYS_ABBR = {"Mon","Tues","Wed","Thurs","Fri","Sat","Sun"};
+}
